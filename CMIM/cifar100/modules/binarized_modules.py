c30f05c822a41fd66643188345f7bbbe9ae72f9d
42Shawn <36666427+42Shawn@users.noreply.github.com>
2022-07-11 18:10:07 -0500

    Add files via upload

---
+++ CMIM/cifar100/modules/binarized_modules.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+import math
+import numpy as np
+import torch.nn.functional as F
+from torch.autograd import Function, Variable
+from scipy.stats import ortho_group
+from utils.options import args
+
+class BinarizeConv2d(nn.Conv2d):
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True):
+        super(BinarizeConv2d, self).__init__(in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias)
+        self.k = torch.tensor([10]).float().cuda()
+        self.t = torch.tensor([0.1]).float().cuda()
+
+    def forward(self, input):
+        w = self.weight
+        a = input
+        bw = w - w.view(w.size(0), -1).mean(-1).view(w.size(0), 1, 1, 1)
+        bw = bw / bw.view(bw.size(0), -1).std(-1).view(bw.size(0), 1, 1, 1)
+        sw = torch.pow(torch.tensor([2] * bw.size(0)).cuda().float(),
+                       (torch.log(bw.abs().view(bw.size(0), -1).mean(-1)) / math.log(2)).round().float()).view(
+            bw.size(0), 1, 1, 1).detach()
+        bw = BinaryQuantize().apply(bw, self.k, self.t)
+        ba = BinaryQuantize().apply(a, self.k, self.t)
+        bw = bw * sw
+        output = F.conv2d(ba, bw, self.bias,
+                          self.stride, self.padding,
+                          self.dilation, self.groups)
+        return output
+
+class BinaryQuantize(Function):
+    @staticmethod
+    #jingtaifangfa make the function can be directly used by .forward(), without instance.
+    def forward(ctx, input, k, t):
+        ctx.save_for_backward(input, k, t)
+        out = torch.sign(input)
+        return out
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        input, k, t = ctx.saved_tensors
+        grad_input = k * t * (1 - torch.pow(torch.tanh(input * t), 2)) * grad_output
+        return grad_input, None, None
+
+
+def get_ab(N):
+    sqrt = int(np.sqrt(N))
+    for i in range(sqrt, 0, -1):
+        if N % i == 0:
+            return i, N // i
